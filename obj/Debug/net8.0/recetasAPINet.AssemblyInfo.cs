f7952a9508a49cb10af09b82e62cdf0184a1b6ba
alvarovr712 <alvarovr712@gmail.com>
2026-03-13 15:59:21 +0100

    test 1

--- obj/Debug/net8.0/recetasAPINet.AssemblyInfo.cs
+++ obj/Debug/net8.0/recetasAPINet.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("recetasAPINet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4fdee90692ef0f7bc42d4cee3e238a57195a6401")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+196921a60312a655af45897d6cbf6a1c76903868")]
 [assembly: System.Reflection.AssemblyProductAttribute("recetasAPINet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("recetasAPINet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
